164200ac87daea6b7c24be62276dab90a0aa4ba2
vairavanthiagarajan <162191296+vairavanthiagarajan@users.noreply.github.com>
2024-03-13 17:02:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var loader = document.getElementById("preloader");
+var navbrandcolor = document.getElementById("navbar-brand");
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+    navbrandcolor.style.color = "whitesmoke";
+})
